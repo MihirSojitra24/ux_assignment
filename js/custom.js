7b6483ffe3f8b5b2bfda976444ae6bacfd560a3e
Mihir Sojitra <76615361+MihirSojitra24@users.noreply.github.com>
2022-06-23 01:51:13 +0530

    upload project

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+/*  rate slider */
+const container = document.querySelector(".slider__box");
+const btn = document.querySelector(".slider__btn");
+const color = document.querySelector(".slider__color");
+const tooltip = document.querySelector(".slider__tooltip");
